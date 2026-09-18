aca2feee6bc4ca414576b85a34d0188138b828ad
Literally Happyfeet <138872496+LeetHappyfeet@users.noreply.github.com>
2026-09-18 15:53:19 -0400

    Remove duplicate semantic classifier version override

--- semantic_index/__init__.py
+++ semantic_index/__init__.py
@@ -15,7 +15,6 @@ from .validation_adapter import (
 
 _original_neighbor_classifier = _neighbor_classifier.classify_neighbor_relations_once
 _original_neighbor_reconciliation = _reconciliation.reconcile_neighbor_relations_once
-_neighbor_classifier.NEIGHBOR_CLASSIFIER_VERSION = "semantic-neighbor-classifier-v4-event-identity"
 _neighbor_classifier.classify_neighbor_pair = _validate_neighbor_relation
 
 
